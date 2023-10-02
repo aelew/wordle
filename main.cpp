925da302444450027f8148d319622ee6219eb2b5
Andre Lew <alew7@ucmerced.edu>
2023-10-02 21:43:39 +0000

    Fixed bug with color classification

--- main.cpp
+++ main.cpp
@@ -3,16 +3,42 @@
 
 using namespace std;
 
+int contains(string word, char c) {
+    for (int i = 0; i < word.length(); i++) {
+        if (word[i] == c) {
+            return i;
+        }
+    }
+    return -1;
+}
+
 int main(int argc, char* argv[]) {
     // TODO: select this randomly from the words.txt
     string solution = "SLATE";
+    string originalSolution = solution;
 
     string attempt;
     cin >> attempt;
 
-    cout << green(attempt[0]) << " ";
-    cout << yellow(attempt[1]) << " ";
-    cout << gray(attempt[2]) << " ";
+    // cout << green(attempt[0]) << " ";
+    // cout << yellow(attempt[1]) << " ";
+    // cout << gray(attempt[2]) << " ";
+    // cout << endl;
+
+    for (int i = 0; i < attempt.length(); i++) {
+        if (attempt[i] == solution[i]) {
+            solution[i] = '-';
+            cout << green(attempt[i]) << " ";
+        } else {
+            int pos = contains(solution, attempt[i]);
+            if (pos != -1) {
+                cout << yellow(attempt[i]) << " ";
+                solution[pos] = '-';
+            } else {
+                cout << gray(attempt[i]) << " ";
+            }
+        }
+    }
     cout << endl;
 
     return 0;
