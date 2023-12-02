b0113ba4fb313752e60736d6367e73d30c171ccb
Andre Lew <alew7@ucmerced.edu>
2023-12-02 01:00:31 +0000

    Print basic keyboard

--- keyboard/main.cpp
+++ keyboard/main.cpp
@@ -1,12 +1,46 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
+string getKeyData(int line, char letter) {
+    if (line == 1) {
+        std::string data = "";
+        data += "| ";
+        data += letter;
+        data += " |";
+        return data;
+    }
+    return " --- ";
+}
 
-    /*
-        your code goes here
-    */
+int main(int argc, char* argv[]) {
+    vector<vector<char>> rows = {
+        { 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P' },
+        { 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L' },
+        { 'Z', 'X', 'C', 'V', 'B', 'N', 'M' }
+    };
+
+    for (int i = 0; i < rows.size(); i++) {
+        vector<char> row = rows[i];
+        int rowLength = row.size();
+
+        for (int j = 0; j < 3; j++) {
+            // Add extra padding to the left
+            if (i == 1) {
+                cout << "  ";
+            } else if (i == 2) {
+                cout << "       ";
+            }
+
+            for (int k = 0; k < row.size(); k++) {
+                char letter = row[k];
+                cout << getKeyData(j, letter);
+            }
+
+            cout << endl;
+        }
+    }
 
     return 0;
 }
